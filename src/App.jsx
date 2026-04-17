cc2ecfa58aa15941af90e78c4752df021305990e
lalithdev <2400031810@kluniversity.in>
2026-04-17 21:15:07 +0530

    Added Vercel Speed Insights

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import AppRoutes from "./routes/AppRoutes";
 import { Toaster } from "sonner";
+import { SpeedInsights } from "@vercel/speed-insights/react";
+
 
 function App() {
   return (
     <>
+      <SpeedInsights />
       <Toaster position="top-right" richColors closeButton />
       <AppRoutes />
     </>
